619af954f790227a21fa7100822204a4ac65930e
Sagar Datkhile <sagar17datkhile@gmail.com>
2025-04-09 23:06:27 +0530

    Reapply "After Popup"
    This reverts commit bdcdac4fb5ddcdb6ae19848d8606a9d4db07a107.

--- Assests/script.js
+++ Assests/script.js
@@ -1,18 +1,2 @@
-// Navigation Bar
-document.getElementById("home").addEventListener("click", () => {
-    window.location.href = "index.html"; 
-    
-});
 
-document.getElementById("about").addEventListener("click", () => {
-    window.location.href= "about.html";
-});
-
-document.getElementById("login").addEventListener("click", () => {
-    window.location.href = "login_page.html"; 
-});
-
-document.querySelector(".get-started").addEventListener("click", () => {
-    window.location.href = "login_page.html"; 
-});
 
