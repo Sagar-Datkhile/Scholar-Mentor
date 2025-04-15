baffdb084c62db1879f80066b413d5fa8bb4740e
Sagar Datkhile <sagar17datkhile@gmail.com>
2025-04-15 10:51:32 +0530

    Changes in file locations

---
+++ Assests/Index-Assets/script.js
@@ -0,0 +1,86 @@
+//  JavaScript 
+
+ new Typed(".introLine2", {
+    strings: ["Career Growth", "Networking Opportunities", "Student-Alumni Engagement"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+});
+
+
+
+    // JavaScript to control popup
+    const popup = document.getElementById("popup");
+    const overlay = document.getElementById("overlay");
+    
+    function showPopup() {
+      popup.style.display = "flex";
+      overlay.style.display = "block";
+    }
+
+    function hidePopup() {
+      popup.style.display = "none";
+      overlay.style.display = "none";
+    }
+
+
+    // Optional: Close popup when pressing ESC key
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            hidePopup();
+        }
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("a[data-target]").forEach(link => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute("data-target");
+
+      // Hide all form containers
+      document.querySelectorAll(".form-container").forEach(form => form.classList.remove("active"));
+
+      // Show the selected form container
+      document.getElementById(targetId).classList.add("active");
+    });
+  });
+
+  // Alumni/Student toggle
+    document.getElementById("passout").addEventListener("change", function() {
+    document.getElementById("passout-fields").classList.remove("hidden");
+    document.getElementById("ongoing-fields").classList.add("hidden");
+  });
+
+    document.getElementById("ongoing").addEventListener("change", function() {
+    document.getElementById("ongoing-fields").classList.remove("hidden");
+    document.getElementById("passout-fields").classList.add("hidden");
+  });
+});
+
+    
+
+ 
+
+    // Navigation Bar
+    function homePage() {
+        window.location.href = "index.html"; 
+    }
+
+    function aboutPage() {
+        window.location.href= "Assests/About-Assets/about.html";
+    }
+
+    document.getElementById("home").addEventListener("click", homePage);
+    document.getElementById("about").addEventListener("click", aboutPage);
+
+    document.addEventListener("DOMContentLoaded", () => {
+    // Check for the flag
+    if (localStorage.getItem("openLoginPopup") === "true") {
+        localStorage.removeItem("openLoginPopup"); // Remove so it doesn't happen again
+        // Trigger the Get Started button
+        document.querySelector("#getStarted").click();
+    }
+});
+
+
+
--- Assests/Login-Assets/script.js
+++ Assests/Login-Assets/script.js
@@ -18,3 +18,4 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("passout-fields").classList.add("hidden");
     });
 });
+
